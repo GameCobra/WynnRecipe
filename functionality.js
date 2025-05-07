af33b9b38ece7dcb9dc806d7c071c144e0f330fb
GameCobra <adrian@houser.ca>
2025-05-07 16:18:22 -0400

    object testing

--- functionality.js
+++ functionality.js
@@ -5,3 +5,29 @@ function myFunction() {
 function notMyFunction() {
     document.getElementById("demo").innerHTML = "Paragraph NOT changed.";
 }
+
+function Person(first = "a", last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+const TestPerson = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+function newPerson()
+{
+    const mySelf = new Person(last="Too", age=22, eye="green");
+    const test = new CreateIngreadientObject();
+    test.firstName = "jim";
+    document.getElementById("demo").innerHTML = test.firstName;
+}
+
+function CreateIngreadientObject() {
+    this.firstName;
+}
