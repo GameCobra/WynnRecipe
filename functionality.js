fd8a88c59c1eba15f7ca60c7c2ed3af1bcbfc31f
GameCobra <adrian@houser.ca>
2025-05-07 15:31:55 -0400

    Nav bar

--- functionality.js
+++ functionality.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+}
+
+function notMyFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph NOT changed.";
+}
